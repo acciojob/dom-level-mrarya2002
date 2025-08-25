1f9023e4b5b9a4de4c87dbce0f24af6566cc18fa
Nikhil Arya <nikhilarya3108202@gmail.com>
2025-08-25 18:23:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const targetElement = document.querySelector("#level");
+    
+    function findLevel(element){
+        let level=0;
+        while(element){
+            element = element.parentElement;
+            level++;
+        }
+        return level;
+    }
+    
+    const getlevel = findLevel(targetElement);
+    alert("The level of the element is: "+getlevel);
